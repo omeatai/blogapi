9dd4c0a45a048be13590badbbe8352a9bc45c237
IFEANYI OMEATA <32337103+omeatai@users.noreply.github.com>
2023-03-01 21:41:35 +0100

    next commit

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)u-++w$^_nauaz*5i4qx84pwowhqa#g*x(ay!(lprcng7#cz#('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['blog-api-ifeanyi.herokuapp.com', '127.0.0.1', 'localhost']
 
